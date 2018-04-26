7eda2fa0ab1a7a8315efddc5b409a8ca980fbaf9
LJanulis <lukas.janulis@gmail.com>
2018-04-26 17:58:31 +0300

    minor fix

--- installation.R
+++ installation.R
@@ -1,6 +1,5 @@
 installRattle <- function(){
-  
-  directory <- choose.dir(default = "", caption = "Please elect RattleM folder");
+  directory <- choose.dir(default = "", caption = "Please select RattleM folder");
   directory <- gsub("\\", "/", directory, fixed=TRUE)
   if(is.na(directory)){
     stop("Incorrect directory selected")
@@ -21,19 +20,22 @@ checkRandJava <- function(){
   javaInfo <- system("java -version", intern = TRUE)
   
   javaVers <- strsplit(javaInfo[3],split=' (build', fixed=TRUE)[[1]][1]
-  print(javaVers)
   rVersion <- R.version$arch
   
+  print(javaInfo)
+  print(javaVers)
+  print(rVersion)
+  
   if(identical(r32Bit, rVersion) && identical(j32Bit, javaVers)){
-    return(TRUE);
+    return(TRUE)
   }
   if(identical(r64Bit, rVersion) && identical(j64Bit, javaVers)){
-    return(TRUE);
+    return(TRUE)
   }
   stop("Installed R and Java versions must be equal! (both 32-bit or both 64-bit)")
 }
 
-checkRandJava()
+#checkRandJava()
 
 if(require("devtools")){
   installRattle()
